d5db34b5eaa7c2f680809f6455bf85827790520e
vadim-sobinin <vadimsobinin@yandex.ru>
2022-10-12 11:58:13 +0300

    Refactor static app

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -48,13 +48,13 @@ const List = ({ loading }) => {
             <path d="M16,1H4v28h22V11L16,1z M16,3.828L23.172,11H16V3.828z M24,27H6V3h8v10h10V27z M8,17h14v-2H8V17z M8,21h14v-2H8V21z M8,25h14v-2H8V25z"></path>{' '}
           </g>
         </svg>
-        Покупки
+        Purchase
       </li>
       <li>
         <svg height="14px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="14px">
           <path d="M28,14H18V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v10H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h10v10c0,1.104,0.896,2,2,2  s2-0.896,2-2V18h10c1.104,0,2-0.896,2-2S29.104,14,28,14z" />
         </svg>
-        Добавить
+        Add
       </li>
     </ul>
   );
